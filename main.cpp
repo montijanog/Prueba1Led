6f5a4208001fd474cd0c4e01d3d3a8638027b472
miguel <j.montijano@opendeusto.es>
2022-11-15 08:48:41 +0100

    prueba

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "mbed.h"
+
+#define WAIT_TIME_MS 500 
+DigitalOut led1 (D3);
+DigitalIn boton (D2);
+
+
+int main()
+{
+    printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
+
+    while (true)
+    {
+        led1 = boton;
+        thread_sleep_for(WAIT_TIME_MS);
+    }
+}
